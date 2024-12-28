25126d96ff2fafb8cc748284d33dcd177f1372db
Nam Gyu Kim <kn89561@mail.harpercollege.edu>
2024-12-28 00:05:23 -0600

    btns function updates

--- game.js
+++ game.js
@@ -8,22 +8,69 @@ import {
   divDisappear,
 } from "./functions.js";
 
+import { oxford3000 } from "./newword.js";
+
 const state = JSON.parse(localStorage.getItem("state"));
 
 document.getElementById(
   "firstH1"
 ).innerText = `"${state.yourInfo.yourName}" 용사님의 여정입니다`;
+
 function letsPlayQuiz() {
-  Object.keys(state.quizInfo).map((el) => {
+  Object.keys(state.quizInfo).map((quizDate) => {
     const newDiv = document.createElement("button");
     newDiv.style.width = "80%";
     newDiv.style.height = "15%";
     newDiv.style.fontSize = "7vw";
-    newDiv.innerText = el;
-    newDiv.id = `quizContainer-${el}`;
+    newDiv.innerText = quizDate;
+    newDiv.id = `quizContainer-${quizDate}`;
     newDiv.addEventListener("click", (event) => {
       const text = event.target.innerText;
-      console.log(state.quizInfo[text]);
+      document.getElementById("quizContainer").innerHTML = "";
+      state.quizInfo[quizDate].quizSelectedWords.map((el, index) => {
+        const singleQuiz = document.createElement("div");
+        singleQuiz.id = String(`${quizDate}-${Object.keys(oxford3000)[el]}`);
+        singleQuiz.style.width = "100%";
+        singleQuiz.style.height = "100%";
+        if (index > 0) {
+          singleQuiz.classList.add("invisible");
+        }
+        singleQuiz.style.display = "flex";
+        singleQuiz.style.justifyContent = "center";
+        singleQuiz.style.alignItems = "center";
+        singleQuiz.style.flexDirection = "column";
+
+        document.getElementById("quizContainer").append(singleQuiz);
+
+        const sigleQuizUpperContainer = document.createElement("div");
+        const sigleQuizLowerContainer = document.createElement("div");
+
+        singleQuiz.append(sigleQuizUpperContainer);
+        singleQuiz.append(sigleQuizLowerContainer);
+
+        sigleQuizUpperContainer.textContent = `${index + 1} : ${
+          Object.keys(oxford3000)[el]
+        }`;
+
+        const prevBtn = document.createElement("button");
+        const nextBtn = document.createElement("button");
+
+        sigleQuizLowerContainer.append(prevBtn);
+        sigleQuizLowerContainer.append(nextBtn);
+
+        prevBtn.textContent = "previous";
+        nextBtn.textContent = "next";
+
+        nextBtn.addEventListener("click", () => {
+          singleQuiz.nextElementSibling.classList.remove("invisible");
+          singleQuiz.classList.add("invisible");
+        });
+
+        prevBtn.addEventListener("click", () => {
+          singleQuiz.previousElementSibling.classList.remove("invisible");
+          singleQuiz.classList.add("invisible");
+        });
+      });
     });
     document.getElementById("quizContainer").append(newDiv);
     divAppear("dungeonContainer");
@@ -35,9 +82,11 @@ function letsPlayQuiz() {
 
 document.getElementById("dungeonBtn").addEventListener("click", () => {
   letsPlayQuiz();
+  console.log(state);
 });
 
 document.getElementById("gobackBtn").addEventListener("click", () => {
+  document.getElementById("quizContainer").innerHTML = "";
   divAppear("settingContainer");
   divAppear("firstH1");
   divDisappear("dungeonContainer");
